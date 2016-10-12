b0a263e4943516ede5601cedddc0f758783bd074
jyazzie79 <jakkieyazzie79@gmail.com>
2016-10-12 15:37:56 -0600

    ajusted navbar links

--- about.php
+++ about.php
@@ -4,10 +4,18 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headutils.php"); ?>
+    <link rel="stylesheet" href="lib/css/styleAbout.css">
   </head>
   <body>
 
-    <?php require_once("lib/partails/navBar.php"); ?>
+    <?php require_once("lib/partials/navBar.php"); ?>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Hello This Is My About Page<h1>
+          <div class="row">
 
+          </div>
+      </div>
+    </div>
   </body>
 </html>
