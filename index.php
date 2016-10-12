4d724f9bd1b4a5db1a28cc144be77f5f60d24783
SimeonAtcitty <atcittysimo@gmail.com>
2016-10-12 15:22:15 -0600

    added jumbotron in index.php
    added jumbotron in index.php

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php");?>
+    <link rel="stylesheet" href="lib/css/index.css">
   </head>
   <body>
 
     <?php require_once("lib/partials/navBar.php");?>
+
+    <div class="container">
+      <div class="jumbotron">
+        <h1>This is my Index page.</h1>
+        <p>Hello you are welcome to browse around.</p>
+        <div class="row">
+          <div class="col-md-3 col-md-offset-9">
+            <button class="btn-lg">LEARN MORE!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
 
 
   </body>
